7fbf7eb9fd3f309c73981bef2e0937b7cb05ff82
Josh Perry <joshperry@outlook.com>
2020-04-24 15:17:35 +0100

    Spinning cube

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -50,47 +50,101 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 
-	//glCullFace(GL_BACK);
-	//glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
 
-	float points[] = {
-		 0.5f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
+	static const GLfloat points[] = {
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
 	};
 
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3
+	static const float colours[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
 	};
 
-	float colours[] = {
-		1.0f, 0.2f, 0.2f,
-		0.2f, 1.0f, 0.2f,
-		0.2f, 0.2f, 1.0f,
-		1.0f, 0.2f, 0.5f
-	};
-
-	unsigned int ebo;
-	glGenBuffers(1, &ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	GLuint points_vbo = 0;
+	GLuint points_vbo;
 	glGenBuffers(1, &points_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-	glBufferData(GL_ARRAY_BUFFER, (sizeof(points)/sizeof(float)) * sizeof(float), points, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (sizeof(points) / sizeof(float)) * sizeof(float), points, GL_STATIC_DRAW);
 
-	GLuint colours_vbo = 0;
+	GLuint colours_vbo;
 	glGenBuffers(1, &colours_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
-	glBufferData(GL_ARRAY_BUFFER, (sizeof(colours)/sizeof(float)) * sizeof(float), colours, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (sizeof(colours) / sizeof(float)) * sizeof(float), colours, GL_STATIC_DRAW);
 
 	const int vao_point_index = 0;
 	const int vao_colour_index = 1;
 
-	GLuint vao = 0;
+	GLuint vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	glEnableVertexAttribArray(vao_point_index);
@@ -122,6 +176,17 @@ int main()
 
 	auto mat = create_material(vertex_shader_src, frag_shader_src);
 
+	const float width = 4;
+	const float height = 3;
+
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), width / height, 0.1f, 100.0f);
+
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(4, 3, 3), // eye
+		glm::vec3(0, 0, 0), // center
+		glm::vec3(0, 1, 0)  // up
+	);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
@@ -135,27 +200,19 @@ int main()
 
 		glUseProgram(mat.shader_program);
 
-		const float width = 4;
-		const float height = 3;
-
-		glm::mat4 projection = glm::perspective(glm::radians(45.0f), width / height, 0.1f, 100.0f);
-
-		glm::mat4 view = glm::lookAt(
-			glm::vec3(4, 3, 3), // eye
-			glm::vec3(0, 0, 0), // center
-			glm::vec3(0, 1, 0)  // up
-		);
-
 		glm::mat4 identity = glm::mat4(1.0f);
 		glm::mat4 transform = projection * view * identity;
 		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-		transform = glm::rotate(transform, (float)glfwGetTime()*2, glm::vec3(1.0f, 0.0f, 0.0f));
+		transform = glm::rotate(transform, (float)glfwGetTime() * 2, glm::vec3(1.0f, 0.0f, 0.0f));
 
 		GLuint transformLoc = glGetUniformLocation(mat.shader_program, "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, sizeof(points) / sizeof(float));
+
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 	}
